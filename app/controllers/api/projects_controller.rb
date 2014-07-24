c45a859c924a7d13437f27de1b735be8aa3899bb
Jesus Garcia <heysusgarcia@gmail.com>
2014-07-24 00:20:03 -0700

    Created ProjectsController

---
+++ app/controllers/api/projects_controller.rb
@@ -0,0 +1,47 @@
+module Api
+  class ProjectsController < ApplicationController
+    def create
+      @project = Project.new(project_params)
+      @project.founder_id = current_user.id
+      if @project.save
+        render json: @project
+      else
+        render json: @project.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @project = Project.find(params[:id])
+      @project.try(:destroy)
+      render json: {}
+    end
+
+    def index
+      @projects = Project.all
+      render json: @projects
+    end
+
+    def show
+      @project = Project.find(params[:id])
+      render json: @project
+    end
+
+    def update
+      @project = Project.find(params[:id])
+      if @project.update_attributes(project_params)
+        render json: @project
+      else
+        render json: @project.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+
+    private
+
+    def project_params
+      params.require(:project).permit(:title, :description, :funding_goal,
+        :amount_raised, :goal_date
+      )
+    end
+  end
+end
